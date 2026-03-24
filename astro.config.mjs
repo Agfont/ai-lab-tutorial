485212cf587abdee298946976accc647046d0c9e
Agfont <arthurfont2000@gmail.com>
2026-03-24 21:46:13 +0100

    ci: add netlify adapter support to handle on demand server rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from "astro/config";
+import netlify from "@astrojs/netlify";
 
 export default defineConfig({
-  server: {
-    port: 4321,
-    host: true
-  }
+  adapter: netlify()
 });
